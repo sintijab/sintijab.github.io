bacadebb22292eb5324e5ec4771702fdc8e4a006
Sintija Birgele <sintija.birgele@friday.de>
2023-10-03 17:09:44 +0200

    fix: config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,5 @@ import svelte from "@astrojs/svelte";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://sintijab.github.io',
-  base: '/',
   integrations: [mdx(), sitemap(), svelte()],
 });
