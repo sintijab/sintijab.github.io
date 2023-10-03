e21e9b4fbd28bb5edf671d63654577d7213b58c1
Sintija Birgele <sintija.birgele@friday.de>
2023-10-03 23:27:33 +0200

    feat: image optimization

--- src/components/Image.svelte
+++ src/components/Image.svelte
@@ -11,9 +11,9 @@
 </script>
 
   {#if media_type === 'image' && !rounded}
-    <img class={`media ${fullWidth ? 'img-fullWidth' : ''}`} src={url} alt={alt} width={width} height={height} />
+    <img loading="lazy" class={`media ${fullWidth ? 'img-fullWidth' : ''}`} src={url} alt={alt} width={width} height={height} />
   {:else if rounded}
-    <img class="clip-rounded" width={width} height={height} src={url} alt={alt} />
+    <img loading="lazy" class="clip-rounded" width={width} height={height} src={url} alt={alt} />
   {:else}
     <!--for videos and other media, embed with an iframe-->
     <iframe class="media" title={title} width="500" height="300" src={url}></iframe>
