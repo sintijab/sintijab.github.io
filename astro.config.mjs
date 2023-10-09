d8b3d390042fd4e5eb85122893d3cfe09ef736fd
sintijab <sintija649@gmail.com>
2023-10-09 20:34:03 +0200

    feat: fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,16 +7,5 @@ import svelte from "@astrojs/svelte";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://syntia.org',
-  server: {
-    headers: {
-      "Content-Security-Policy": " style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline' *.googletagmanager.com *.prismic.io *.amazonaws.com *.google-analytics.com *.youtube.com; img-src 'self' data: *.prismic.io; default-src 'self' *.googletagmanager.com *.prismic.io *.amazonaws.com *.google-analytics.com *.youtube.com",
-      "X-Content-Type-Options": "nosniff",
-      "Referrer-Policy": "strict-origin-when-cross-origin",
-      "Permissions-Policy": "geolocation=(self)",
-      "X-Frame-Options": "DENY",
-      "Strict-Transport-Security": "max-age=63072000; includeSubDomains; preload",
-      "Access-Control-Allow-Origin": "https://syntia.org"
-    }
-  },
   integrations: [mdx(), sitemap(), svelte()]
 });
