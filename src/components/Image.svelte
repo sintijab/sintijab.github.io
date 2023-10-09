4c44fe441e5d5403d51f7867333265c56d45ca84
sintijab <sintija649@gmail.com>
2023-10-09 17:17:05 +0200

    feat: optimize imports

--- src/components/Image.svelte
+++ src/components/Image.svelte
@@ -11,12 +11,12 @@
 </script>
 
   {#if media_type === 'image' && !rounded}
-    <img loading="lazy" class={`media ${fullWidth ? 'img-fullWidth' : ''}`} src={url} alt={alt} width={width} height={height} />
+    <img client:visible loading="lazy" class={`media ${fullWidth ? 'img-fullWidth' : ''}`} src={url} alt={alt} width={width} height={height} />
   {:else if rounded}
-    <img loading="lazy" class="clip-rounded" width={width} height={height} src={url} alt={alt} />
+    <img client:visible loading="lazy" class="clip-rounded" width={width} height={height} src={url} alt={alt} />
   {:else}
     <!--for videos and other media, embed with an iframe-->
-    <iframe class="media" title={title} width="500" height="300" src={url}></iframe>
+    <iframe client:visible class="media" title={title} width="500" height="300" src={url}></iframe>
   {/if}
 <style>
   .media {
